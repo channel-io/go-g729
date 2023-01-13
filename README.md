ce40833c380be12726a9127448070cb6030688ae
maxjung <max@channel.io>
2023-01-13 14:43:53 +0900

    G729 encoder/decoder with cgo execution (#1)

---
+++ README.md
@@ -0,0 +1,12 @@
+## Go wrapper for G.729
+
+This package provides Go bindings for the BelledonneCommunications C library bcg729.
+
+The C libraries and docs are provided in https://github.com/BelledonneCommunications/bcg729.
+This package just handles the wrapping in Go, and is unaffiliated with xiph.org.
+
+### Prerequisites
+
+Install the C library bcg729 in  https://github.com/BelledonneCommunications/bcg729.
+You have to install the library, and add the path library installed to environment variable.
+
---
+++ examples/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"gitub.com/channel-io/go-g729/pkg"
+)
+
+func main() {
+	// encoding example
+	inputWAV, err := os.Open("./assets/input.wav")
+	if err != nil {
+		panic(err)
+	}
+
+	wavHeader := make([]byte, 44)
+	if _, err = inputWAV.Read(wavHeader); err != nil {
+		panic(err)
+	}
+
+	outG729, _ := os.Create("./enc.g729")
+	if err = encode(inputWAV, outG729); err != nil {
+		panic(fmt.Sprintf("failed to encode: %v", err))
+	}
+
+	if err = inputWAV.Close(); err != nil {
+		panic(err)
+	}
+	if err = outG729.Close(); err != nil {
+		panic(err)
+	}
+
+	// decoding example
+	inputG729, err := os.Open("./enc.g729")
+	if err != nil {
+		panic(err)
+	}
+
+	outWAV, err := os.Create("./dec.wav")
+	if err != nil {
+		panic(err)
+	}
+
+	if _, err = outWAV.Write(wavHeader); err != nil {
+		panic(err)
+	}
+
+	if err = decode(inputG729, outWAV); err != nil {
+		panic(fmt.Sprintf("failed to decode: %v", err))
+	}
+}
+
+func encode(r io.Reader, w io.Writer) error {
+	enc := pkg.NewEncoder(false)
+
+	for {
+		buf := make([]byte, 160)
+		if n, err := r.Read(buf); err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		} else if n != 160 {
+			// ignore last frame if frame size is invalid
+			break
+		}
+
+		if encoded, err := enc.Encode(buf); err != nil {
+			return err
+		} else if _, err = w.Write(encoded); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func decode(r io.Reader, w io.Writer) error {
+	dec := pkg.NewDecoder()
+
+	for {
+		buf := make([]byte, 10)
+		if n, err := r.Read(buf); err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		} else if n != 10 {
+			// ignore last frame if frame size is invalid
+			break
+		}
+
+		if decoded, err := dec.Decode(buf); err != nil {
+			return err
+		} else if _, err = w.Write(decoded); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
